69c0daa5ebb6c52e9f7c1541ad7fd9c72579ea77
Fernando Serena <fernando.serena@centeropenmiddleware.com>
2015-07-17 09:50:19 +0200

    New script for getting the contributed repos of a dev

--- sdh/metrics/store/scm.py
+++ sdh/metrics/store/scm.py
@@ -24,11 +24,12 @@
 
 __author__ = 'Fernando Serena'
 
-
 from sdh.metrics.store.fragment import FragmentStore
 import calendar
 from datetime import datetime
 import uuid
+import re
+
 
 class SCMStore(FragmentStore):
     def __init__(self, redis_host):
@@ -36,14 +37,30 @@ class SCMStore(FragmentStore):
 
     def get_repositories(self):
         repo_keys = self.db.keys('frag:repos:-*-:')
-        repo_data = [{'name': self.db.hget(rk, 'name'), 'uri': rk.lstrip('frag:repos:-').rstrip('-:')}
-                     for rk in repo_keys]
-        return repo_data
+        return filter(lambda x: x is not None, [self.db.hget(rk, 'id') for rk in repo_keys])
 
     def get_repository_branches(self, rid):
         branch_set_keys = self.db.keys('frag:repos:-*-:branches')
         repo_data = [{'name': self.db.hget(bsk, 'name'), 'uri': bsk.split(':')[1]} for bsk in branch_set_keys]
         return repo_data
+
+    def get_commits_repos(self, commits):
+        temp_key = str(uuid.uuid4())
+        self.db.sadd(temp_key, *commits)
+        all_branches_keys = self.db.keys('frag:repos:-*-:branches')
+        repos = set([])
+        pattern = re.compile(r':-(.+?)-:')
+        for b_key in all_branches_keys:
+            r_uri = pattern.findall(b_key).pop()
+            rid = self.db.hget('frag:repos:-{}-:'.format(r_uri), 'id')
+            if rid not in repos:
+                repo_branches = self.db.smembers(b_key)
+                for b_uri in repo_branches:
+                    branch_commits_key = 'frag:branches:-{}-:commits'.format(b_uri)
+                    if self.db.sinter(temp_key, branch_commits_key):
+                        repos.add(rid)
+        self.db.delete(temp_key)
+        return repos
 
     def get_commits(self, begin=0, end=None, rid=None, bid=None, uid=None, withstamps=False, limit=None, start=None):
         if end is None:
@@ -83,7 +100,8 @@ class SCMStore(FragmentStore):
             if rid is not None:
                 temp_key = str(uuid.uuid4())
                 self.db.sadd(temp_key, *branches)
-                branches = self.db.sinter('frag:repos:-{}-:branches'.format(rid), temp_key)
+                r_uri = self.db.get('frag:repos:{}:'.format(rid))
+                branches = self.db.sinter('frag:repos:-{}-:branches'.format(r_uri), temp_key)
                 self.db.delete(temp_key)
 
         return branches
@@ -92,9 +110,9 @@ class SCMStore(FragmentStore):
         commits = self.get_commits(begin, end, rid=rid, withstamps=withstamps, limit=limit, start=start)
         if len(commits):
             developers = set([self.db.hget('frag:commits:-{}-'.format(c), 'by') for c in commits])
-            developers = filter(lambda x: x is not None, developers)
-            developers = [(d_uri, self.db.hget('frag:devs:-{}-'.format(d_uri), 'id')) for d_uri in developers]
-            return developers
+            developers = [self.db.hget('frag:devs:-{}-'.format(d_uri), 'id') for d_uri in developers]
+            return filter(lambda x: x is not None, developers)
+
         return commits
 
     @property
