00e45a06c7dc74779d7b22eb41919b9c36ca148c
Fernando Serena <fernando.serena@centeropenmiddleware.com>
2015-07-17 09:49:03 +0200

    Added required metrics and TBDs for the demo

--- sdh/metrics/scm/api.py
+++ sdh/metrics/scm/api.py
@@ -49,6 +49,12 @@ def get_user_commits(uid, **kwargs):
     return list(store.get_commits(kwargs['begin'], kwargs['end'], uid=uid))
 
 
+@app.usertbd('/user-repositories', 'repositories')
+def get_user_repositories(uid, **kwargs):
+    commits = store.get_commits(kwargs['begin'], kwargs['end'], uid=uid)
+    return list(store.get_commits_repos(commits))
+
+
 @app.userrepotbd('/user-repo-commits', 'commits')
 def get_user_repo_commits(rid, uid, **kwargs):
     return list(store.get_commits(kwargs['begin'], kwargs['end'], uid=uid, rid=rid))
@@ -78,22 +84,45 @@ def get_total_org_commits(**kwargs):
                      kwargs['max'])
 
 
+@app.orgmetric('/total-repositories', 'sum', 'repositories')
+def get_total_org_repositories(**kwargs):
+    return {}, [len(store.get_repositories())]
+
+
 @app.usermetric('/total-user-commits', 'sum', 'commits')
 def get_total_user_commits(uid, **kwargs):
     return aggregate(store, 'metrics:total-user-commits:{}'.format(uid), kwargs['begin'], kwargs['end'],
                      kwargs['max'])
 
 
+@app.repousermetric('/total-repo-user-commits', 'sum', 'commits')
+def get_total_repo_user_commits(rid, uid, **kwargs):
+    return aggregate(store, 'metrics:total-repo-user-commits:{}:{}'.format(rid, uid), kwargs['begin'], kwargs['end'],
+                     kwargs['max'])
+
+
+@app.repousermetric('/avg-repo-user-commits', 'avg', 'commits')
+def get_avg_repo_user_commits(rid, uid, **kwargs):
+    return aggregate(store, 'metrics:total-repo-user-commits:{}:{}'.format(rid, uid), kwargs['begin'], kwargs['end'],
+                     kwargs['max'], aggr=avg)
+
+
+@app.usermetric('/avg-user-commits', 'avg', 'commits')
+def get_avg_user_commits(uid, **kwargs):
+    return aggregate(store, 'metrics:total-user-commits:{}'.format(uid), kwargs['begin'], kwargs['end'],
+                     kwargs['max'], aggr=avg)
+
+
 @app.repometric('/avg-repo-commits', 'avg', 'commits')
 def get_avg_repo_commits(rid, **kwargs):
     return aggregate(store, 'metrics:total-repo-commits:{}'.format(rid), kwargs['begin'], kwargs['end'],
-                     kwargs['max'], aggr=avg)
+                     kwargs['max'], aggr=avg, extend=True)
 
 
 @app.orgmetric('/avg-commits', 'avg', 'commits')
 def get_avg_org_commits(**kwargs):
     return aggregate(store, 'metrics:total-commits', kwargs['begin'], kwargs['end'],
-                     kwargs['max'], aggr=avg)
+                     kwargs['max'], aggr=avg, extend=True)
 
 
 @app.orgmetric('/total-branches', 'sum', 'branches')
@@ -116,22 +145,36 @@ def get_avg_org_branches(**kwargs):
 
 @app.orgmetric('/total-developers', 'sum', 'developers')
 def get_total_org_developers(**kwargs):
-    def __aggr(x):
-        if any(isinstance(el, list) for el in x):
-            chain = itertools.chain(*x)
-            return len(set(list(chain)))
-        else:
-            return sum(x)
+    def aggr_whole(x):
+        return [len(elm) for elm in x]
 
-    return aggregate(store, 'metrics:total-developers', kwargs['begin'], kwargs['end'],
-                     kwargs['max'], __aggr)
+    def __aggr(x):
+        chain = itertools.chain(*list(x))
+        return len(set(list(chain)))
+
+    aggr = __aggr
+    if not kwargs['max']:
+        aggr = aggr_whole
+
+    context, result = aggregate(store, 'metrics:total-developers', kwargs['begin'], kwargs['end'],
+                                kwargs['max'], aggr, fill=[])
+    if aggr == aggr_whole:
+        result = result.pop()
+    return context, result
 
 
 @app.repometric('/total-repo-developers', 'sum', 'developers')
 def get_total_repo_developers(rid, **kwargs):
+    def aggr_whole(x):
+        return x
+
     def __aggr(x):
         chain = itertools.chain(*x)
         return len(set(list(chain)))
 
+    aggr = __aggr
+    if not kwargs['max']:
+        aggr = aggr_whole
+
     return aggregate(store, 'metrics:total-repo-developers:{}'.format(rid), kwargs['begin'], kwargs['end'],
-                     kwargs['max'], __aggr)
+                     kwargs['max'], aggr, fill=[])
